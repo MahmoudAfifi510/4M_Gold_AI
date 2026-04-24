304a49349174293e9d9b189b9d4a02eb13a4ccfb
MahmoudAfifi510 <mahmouduniversity2024@gmail.com>
2026-04-24 18:05:00 +0300

    Delete backend directory

--- backend/app/schemas/market.py
+++
@@ -1,9 +0,0 @@
-from datetime import date
-from pydantic import BaseModel
-
-
-class MarketDataRead(BaseModel):
-    market_date: date
-    gold_price: float
-    oil_price: float
-    usd_index: float
